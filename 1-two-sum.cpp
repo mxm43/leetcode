5d130667e42a7cccc34ba1d9679d6482b31f3532
mxm43 <maxime.limy@gmail.com>
2017-03-02 15:22:50 +0800

    add 1-two-sum.cpp

---
+++ 1-two-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        vector<int> ret_value;
+        vector<int>::iterator it;
+
+        vector< pair<int,int> > v;
+        for (it = nums.begin(); it != nums.end(); ++it)
+            v.push_back(make_pair(*it, it - nums.begin()));
+        sort (v.begin(), v.end(),
+            [](pair<int,int>& a, pair<int,int>& b){return a.first < b.first;});
+
+        vector< pair<int,int> >::iterator i,j;
+        for (i = v.begin(); i != v.end()-1; ++i)
+        {
+            for (j = i+1; j != v.end() && (*i).first + (*j).first < target; ++j);
+            if ((*i).first + (*j).first == target)
+                break;
+        }
+
+        ret_value.push_back((*i).second);
+        ret_value.push_back((*j).second);
+
+        return ret_value;
+    }
+};
+
