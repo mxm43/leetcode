88ad5240f723208e9f93ca7b94841f6b717a1963
mxm43 <maxime.limy@gmail.com>
2017-03-14 23:58:47 +0800

    7-reverse-integer.cpp

---
+++ 7-reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int max = ~0 & ~(1 << 31), min = (1 << 31);
+        long ret = 0;
+
+        int tmp = x;
+        while (tmp != 0)
+        {
+            ret = ret*10 + tmp%10;
+            tmp /= 10;
+        }
+        if (ret > max || ret < min)
+            return 0;
+        else
+            return ret >= 0 ? (int)ret : (int)ret | (1 << 31);
+    }
+};
+
