abd20bd7d58cf3a7caff3d3befd1d10070db79ff
mxm43 <maxime.limy@gmail.com>
2017-03-16 19:44:49 +0800

    add 8-string-to-integer.cpp

---
+++ 8-string-to-integer.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int myAtoi(string str) {
+        if (str.size() == 0) return 0;
+
+        int  i = 0;
+        while (i<str.size() && isspace(str[i])) ++i;
+        if (i >= str.size()) return 0;
+
+        bool neg = str[i]=='-' ? true : false;
+        long ret = 0;
+
+        if    (str[i]=='+' || str[i]=='-')  ++i;
+        for (; i < str.size() && isdigit (str[i]); ++i)
+        {
+            int d = str[i]-'0';
+            if (neg == true)
+            {
+                ret = ret*10 - d;
+                if (ret < INT_MIN)
+                    return INT_MIN;
+            }
+            else
+            {
+                ret = ret*10 + d;
+                if (ret > INT_MAX)
+                    return INT_MAX;
+            }
+        }
+
+        return ret;
+    }
+};
+
