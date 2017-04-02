cf49a8d49c548cd320680fc6c731aa897d4ae055
mxm43 <maxime.limy@gmail.com>
2017-04-02 17:48:29 +0800

    add 25-reverse-nodes-in-k-group.cpp

---
+++ 25-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    ListNode* reverseKGroup(ListNode* head, int k)
+    {
+        if (k <= 1 || head == NULL) return head;
+
+        ListNode  *rest    = head;
+        ListNode **reverse = &head;
+        while (rest != NULL)
+        {
+            ListNode *node = rest;
+            for (int i = 0; i < k; ++i)
+            {
+                if (rest == NULL)
+                    return head;
+                else
+                    rest = rest->next;
+            }
+
+            ListNode *cur = *reverse;
+            ListNode *result = NULL;
+            for (int i = 0; i < k; ++i)
+            {
+                ListNode *next = cur->next;
+                cur->next = result;
+                result = cur;
+                cur = next;
+            }
+            *reverse = result;
+            node->next = rest;
+            reverse = &(node->next);
+        }
+
+        return head;
+    }
+};
+
