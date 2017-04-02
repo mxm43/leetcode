65bd87664f5ce5a451d8ab1e478d7229b33c16a2
mxm43 <maxime.limy@gmail.com>
2017-04-02 18:43:37 +0800

    add 24-swap-nodes-in-pairs.cpp

---
+++ 24-swap-nodes-in-pairs.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    ListNode* swapPairs(ListNode* head)
+    {
+        ListNode *ret = NULL, *cur = head;
+        ListNode **insert = &ret;
+
+        while (cur != NULL)
+        {
+            ListNode *node = cur;
+            if (node->next != NULL)
+            {
+                cur = (node->next)->next;
+                *insert = node->next;
+                (node->next)->next = node;
+                insert = &(node->next);
+                *insert = NULL;
+            }
+            else
+            {
+                *insert = node;
+                return ret;
+            }
+        }
+
+        return ret;
+    }
+};
+
