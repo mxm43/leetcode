46f8b00a2b25cb037432d3a7240c15e451988368
mxm43 <maxime.limy@gmail.com>
2017-04-15 00:50:29 +0800

    add 29-divide-two-integers.cpp

---
+++ 29-divide-two-integers.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int divide(int dividend, int divisor)
+    {
+        if (divisor==0 || (dividend==INT_MIN && divisor==-1))
+            return INT_MAX;
+
+        bool neg = (dividend^divisor)>>31 ? true : false;
+        int r = 0, c = 0;
+
+        if (dividend > 0)
+            dividend = -dividend;
+        if (divisor  > 0)
+            divisor  = -divisor;
+
+        while (divisor & 1<<30 && dividend<divisor)
+            ++c, divisor <<= 1;
+        for (; c >= 0; --c, divisor >>= 1)
+        {
+            if (dividend <= divisor)
+            {
+                dividend -= divisor;
+                r += 1 << c;
+            }
+        }
+
+        return neg ? -r : r;
+    }
+};
+
