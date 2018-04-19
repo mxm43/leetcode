9f7364c437e278d36d1c957131d8de50f989eb79
mxm43 <maxime.limy@gmail.com>
2018-04-19 20:37:39 +0800

    add 58-length-of-last-word.cpp

---
+++ 58-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        int lengthOfLastWord(string s) {
+            int count = 0;
+            for(auto iter = s.rbegin(); iter != s.rend(); ++iter) {
+                if (*iter == ' ' && count == 0) {
+                    continue;
+                } else if (*iter == ' '){
+                    break;
+                } else {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+};
+
