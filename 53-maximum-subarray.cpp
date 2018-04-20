3deb019efcb091d1bcb6c744ee25343e6ce151ca
mxm43 <maxime.limy@gmail.com>
2018-04-21 00:46:28 +0800

    add 53-maximum-subarray.cpp

---
+++ 53-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums)
+    {
+        int size = nums.size();
+        int ret = nums[size-1], sum = 0;
+
+        for (int i = size-1; i >= 0; --i)
+        {
+            sum = max(nums[i], nums[i] + sum);
+            if (sum > ret)
+                ret = sum;
+        }
+
+        return ret;
+    }
+};
+
