d15848f2f69af8b977361307cf40fb796c612507
mxm43 <maxime.limy@gmail.com>
2018-05-01 21:26:21 +0800

    62-unique-paths.cpp

---
+++ 62-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int uniquePaths(int m, int n)
+    {
+        vector<int> dp(n, 0);
+
+        dp[0] = 1;
+        for (int i = 0; i < m; ++i)
+            for (int j = 1; j < n; ++j)
+                dp[j] += dp[j-1];
+
+        return dp[n-1];
+    }
+};
+
