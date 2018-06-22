93ff0d4b30f9b6cc17a2b7110c13f0a718d1d601
mxm43 <maxime.limy@gmail.com>
2018-06-22 16:34:28 +0800

    add 86-partition-list.cpp

---
+++ 86-partition-list.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    ListNode* partition(ListNode* head, int x)
+    {
+        ListNode *l = NULL, *r = NULL;
+        ListNode **lp = &l, **rp = &r;
+
+        while (head != NULL)
+        {
+            ListNode ***cur = head->val < x ? &lp : &rp;
+
+            *(*cur) = head;
+            head = head->next;
+            *cur = &((*(*cur))->next);
+        }
+
+        *rp = NULL;
+        *lp = r;
+
+        return l;
+    }
+};
+
